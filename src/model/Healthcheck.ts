e2d89bababbb6c5efa07eb1c2b8be0700bde78de
Štefan Baňkos <sbankos1@student.euba.sk>
2022-12-17 17:08:51 +0100

    Zadanie 3

--- src/model/Healthcheck.ts
+++ src/model/Healthcheck.ts
@@ -1,12 +1,12 @@
-import {Company} from './Company';
+import {School} from './School';
 import {Person} from './Person';
 
 export class Healthcheck {
     private author: Person;
-    private company: Company;
+    private company: School;
     private identifier: string;
 
-    constructor(author: Person, company: Company, identifier: string) {
+    constructor(author: Person, company: School, identifier: string) {
         this.author = author;
         this.company = company;
         this.identifier = identifier;
