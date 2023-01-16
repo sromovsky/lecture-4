40bee77efd4510a826ed1a0c253c995aca0f4ef4
filipozvat <116357463+filipozvat@users.noreply.github.com>
2023-01-16 22:37:07 +0100

    Add files via upload

--- src/index.ts
+++ src/index.ts
@@ -1,58 +1,68 @@
-import {Request, Response} from 'express';
-import {Service} from './service/service';
-import {SERVER_PORT} from './const/api.const';
-import {TimeService} from './service/time.service';
-import {MeetingService} from './service/meeting.service';
-import {NewMeeting} from './model/NewMeeting';
+import { Request, Response } from "express";
+import { Service } from "./service/service";
+import { SERVER_PORT } from "./const/api.const";
+import { TimeService } from "./service/time.service";
+import { MeetingService } from "./service/meeting.service";
+import { NewMeeting } from "./model/NewMeeting";
+import { ChronoUnit, LocalDate, LocalTime } from "@js-joda/core";
 
-const express = require('express');
-const service = new Service('1.0.0');
+const express = require("express");
+const service = new Service("1.0.0");
 const timeService = new TimeService();
 const meetingService = new MeetingService();
 
 const app = express();
 app.use(express.json());
+``;
 
-
-app.get('/', (req: Request, res: Response) => {
-    res.send(service.healthcheck());
+app.get("/", (req: Request, res: Response) => {
+  res.send(service.healthcheck());
 });
 
-app.get('/today', (req: Request, res: Response) => {
-    res.send(timeService.today());
+app.get("/today", (req: Request, res: Response) => {
+  res.send(timeService.today());
 });
 
-app.get('/working-time', (req: Request, res: Response) => {
-    res.send(timeService.workingTime());
+app.get("/working-time", (req: Request, res: Response) => {
+  res.send(timeService.workingTime());
 });
 
-app.get('/meetings', (req: Request, res: Response) => {
-    res.send(meetingService.getAll());
+app.get("/meetings", (req: Request, res: Response) => {
+  res.send(meetingService.getAll());
 });
 
-app.post('/meetings', (req: Request, res: Response) => {
-    const newMeeting = new NewMeeting(req.body?.name, req.body?.start);
-
-    if(!meetingService.checkWorkingTime(newMeeting.getStart())){
-        res.send({
-            error: 'Time is out of working time!'
-        });
-    }
-    if (meetingService.hasStartTime(newMeeting.getStart())) {
-        res.statusCode = 409;
-        res.send({
-            error: 'Meeting already exist!'
-        });
-    } else {
-        res.send({id: meetingService.add(newMeeting)});
-    }
+app.get("/meetings/:day", (req: Request, res: Response) => {
+  res.send(meetingService.getAllForDay(req.params.day));
 });
 
-app.get('/free-meeting-times', (req: Request, res: Response) => {
-    res.send(meetingService.freeMeetingTimes());
+app.post("/meetings", (req: Request, res: Response) => {
+  // YYYY-MM-DD
+  const date = req.body?.date
+    ? LocalDate.parse(req.body.date)
+    : LocalDate.now();
+
+  const newMeeting = new NewMeeting(req.body?.name, req.body?.start, date);
+
+  const meetingExists = meetingService.hasStartTime(newMeeting.getStart(), newMeeting.getDate().dayOfMonth());
+
+  if (meetingExists) {
+    res.statusCode = 409;
+    res.send({
+      error: "Meeting already exist!",
+    });
+  } else {
+    res.send({ id: meetingService.add(newMeeting) });
+  }
+});
+
+app.get("/next-time", (req: Request, res: Response) => {
+  const nextTime = LocalTime.now().plusHours(1);
+  res.send(nextTime.truncatedTo(ChronoUnit.HOURS));
 });
 
 // Start server:
 app.listen(SERVER_PORT, () => {
-    console.log(`⚡ [server]: Server is running at http://localhost:${SERVER_PORT}`)
+  console.log(
+    `⚡ [server]: Server is running at http://localhost:${SERVER_PORT}`
+  );
 });
